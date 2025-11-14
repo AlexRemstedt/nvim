1189387084d100b22c9d23b772a8a0e36e89149b
Remstedt, Alex <arem@allseas.com>
2025-11-14 13:22:01 +0100

    feat: map checkbox checker

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,2 +1,17 @@
 vim.o.textwidth = 80
 vim.o.colorcolumn = "81"
+
+vim.keymap.set("n", "<localleader>x", function()
+  local line = vim.api.nvim_get_current_line()
+  local new_line
+  if line:match("%- %[ %]") then
+    -- Unchecked -> Checked
+    new_line = line:gsub("%- %[ %]", "- [X]", 1)
+  elseif line:match("%- %[X%]") then
+    -- Checked -> Unchecked
+    new_line = line:gsub("%- %[X%]", "- [ ]", 1)
+  else
+    return
+  end
+  vim.api.nvim_set_current_line(new_line)
+end, { desc = "Toggle markdown checkbox" })
