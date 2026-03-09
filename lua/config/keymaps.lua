fb4cec1e201172fa2f67b5d8ebf797a0910f8b31
Remstedt, Alex <arem@allseas.com>
2026-03-09 15:40:59 +0100

    keymap: reset j and k

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.del("n", "j")
+vim.keymap.del("n", "k")
+
 vim.keymap.set("n", "<leader>gc", "<cmd>Git commit -v<cr>", { desc = "Git Commit" })
 if vim.fn.executable("lazygit") == 1 then
   vim.keymap.set("n", "<leader>GG", function()
