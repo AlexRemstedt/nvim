0e1dec325208e8b5522a12265bb1c9054bdee60a
Remstedt, Alex <arem@allseas.com>
2026-05-04 11:25:39 +0200

    feat(tmux-sessionizer): integrate sessionizer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,14 @@
 vim.keymap.del("n", "j")
 vim.keymap.del("n", "k")
 
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
+vim.keymap.set("n", "<M-l>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
+
 vim.keymap.set("n", "<leader>gc", "<cmd>Git commit -v<cr>", { desc = "Git Commit" })
-vim.keymap.set("n", "<leader>ga", "<cmd>Git commit --amend<cr>", { desc = "Git amend" })
+vim.keymap.set("n", "<leader>ga", "<cmd>Git commit --amend<cr>", { desc = "Git Amend" })
 if vim.fn.executable("lazygit") == 1 then
   vim.keymap.set("n", "<leader>GG", function()
     Snacks.lazygit()
