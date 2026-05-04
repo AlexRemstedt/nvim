034ad530c124c3ba92ed52207bc620ed3cefbe3a
Remstedt, Alex <arem@allseas.com>
2026-05-04 15:21:59 +0200

    feat(diff): keep cursor in centre when in diff mode.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,6 +8,28 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 vim.cmd("command! W w")
 
+-- Keep cursor centered in diff mode
+local function sync_diff_scrolloff()
+  if vim.wo.diff then
+    if vim.w.diff_saved_scrolloff == nil then
+      vim.w.diff_saved_scrolloff = vim.wo.scrolloff
+    end
+    vim.wo.scrolloff = 999
+  elseif vim.w.diff_saved_scrolloff ~= nil then
+    vim.wo.scrolloff = vim.w.diff_saved_scrolloff
+    vim.w.diff_saved_scrolloff = nil
+  end
+end
+
+vim.api.nvim_create_autocmd({ "OptionSet" }, {
+  pattern = "diff",
+  callback = sync_diff_scrolloff,
+})
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "VimEnter" }, {
+  callback = sync_diff_scrolloff,
+})
+
 -- Auto-restore persistence session on startup if no files were passed as args
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
