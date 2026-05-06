7feee87336e3f3347678a0e068aa4bbf5304e898
Remstedt, Alex <49374085+AlexRemstedt@users.noreply.github.com>
2026-05-06 20:58:17 +0200

    feat(slime): add slime

---
+++ lua/plugins/slime.lua
@@ -0,0 +1,18 @@
+-- lua/plugins/slime.lua
+return {
+  "jpalardy/vim-slime",
+  init = function()
+    vim.g.slime_target = "tmux"
+    vim.g.slime_default_config = {
+      socket_name = "default",
+      target_pane = "{last}",
+    }
+    vim.g.slime_python_ipython = 1
+    vim.g.slime_dont_ask_default = 0
+    vim.g.slime_cell_delimiter = "^```"
+    vim.g.slime_no_mappings = false
+  end,
+  keys = {
+    { "<leader>cc", "<Plug>SlimeSendCell", desc = "Slime send cell", mode = { "n", "x" } },
+  },
+}
